365e55b0e3ed159b0e7379578cfde08ef300d533
void269 <void269.269@gmail.com>
2026-08-13 14:53:30 -0400

    add deployment script for Azure Web App and VM configuration

--- build.sh
+++ build.sh
@@ -9,3 +9,12 @@ az group create \
     --name "$RESOURCE_GROUP" \
     --location "$LOCATION"
 
+echo "Deploying Azure Web App: $WEB_APP_NAME"
+az vm create \
+    --resource-group "$RESOURCE_GROUP" \
+    --name "$VM1_NAME" \
+    --nics "$NIC1_NAME" \
+    --image "$VM_IMAGE" \
+    --size "$VM_SIZE" \
+    --admin-username "$ADMIN_USERNAME" \
+    --generate-ssh-keys
