1f2cab43f2e80ec4009266dee2dcf0f70422b690
void269 <void269.269@gmail.com>
2026-08-13 16:38:07 -0400

    added network setup with arm

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+source ./config.env
+
+echo "Creating Resource Group..."
+az group create \
+    --name "$RESOURCE_GROUP" \
+    --location "$LOCATION"
+
+echo "Creating Virtual Network..."
+az deployment group create \
+    --name "NetworkDeployment" \
+    --resource-group "$RESOURCE_GROUP" \
+    --template-file "./arm/network.json" \
+    --parameters \
+        location="$LOCATION" \
+        vnetName="$VNET_NAME" \
+        vnetAddressPrefix="$VNET_ADDRESS_PREFIX" \
+        subnetName="$SUBNET_NAME" \
+        subnetPrefix="$SUBNET_PREFIX" \
+        nsgName="$NSG_NAME"
+
--- destroy.sh
+++ destroy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
 source ./config.env
 
-echo "Deleting Resource Group: $RESOURCE_GROUP"
+echo "Deleting all resources asssociated with Resource Group: $RESOURCE_GROUP."
 az group delete \
     --name "$RESOURCE_GROUP" \
     --yes \
-    --no-wait \
-    > /dev/null
+    --no-wait
+echo
+echo "Deletion complete."
